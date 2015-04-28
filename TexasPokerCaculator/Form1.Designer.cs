f3b559c0c2fcb5b7817f15be5978057bb77ac293
Ben Huang <aloneplayer@gmail.com>
2015-04-28 18:52:04 -0400

    Create Project
    Create Project

---
+++ TexasPokerCaculator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TexasPokerCaculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_Control = new System.Windows.Forms.Panel();
+            this.pictureBox_PokerStack = new System.Windows.Forms.PictureBox();
+            this.panel_Table = new System.Windows.Forms.Panel();
+            this.pictureBox_Table = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_Control.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_PokerStack)).BeginInit();
+            this.panel_Table.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_Control
+            // 
+            this.panel_Control.Controls.Add(this.label1);
+            this.panel_Control.Controls.Add(this.pictureBox_PokerStack);
+            this.panel_Control.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_Control.Location = new System.Drawing.Point(0, 0);
+            this.panel_Control.Name = "panel_Control";
+            this.panel_Control.Size = new System.Drawing.Size(255, 440);
+            this.panel_Control.TabIndex = 2;
+            // 
+            // pictureBox_PokerStack
+            // 
+            this.pictureBox_PokerStack.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_PokerStack.Image = global::TexasPokerCaculator.Properties.Resources.SmallPokers;
+            this.pictureBox_PokerStack.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_PokerStack.Name = "pictureBox_PokerStack";
+            this.pictureBox_PokerStack.Size = new System.Drawing.Size(255, 440);
+            this.pictureBox_PokerStack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_PokerStack.TabIndex = 4;
+            this.pictureBox_PokerStack.TabStop = false;
+            // 
+            // panel_Table
+            // 
+            this.panel_Table.Controls.Add(this.pictureBox_Table);
+            this.panel_Table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Table.Location = new System.Drawing.Point(255, 0);
+            this.panel_Table.Name = "panel_Table";
+            this.panel_Table.Size = new System.Drawing.Size(677, 440);
+            this.panel_Table.TabIndex = 3;
+            // 
+            // pictureBox_Table
+            // 
+            this.pictureBox_Table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_Table.Image = global::TexasPokerCaculator.Properties.Resources.PokerTable;
+            this.pictureBox_Table.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_Table.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox_Table.Name = "pictureBox_Table";
+            this.pictureBox_Table.Size = new System.Drawing.Size(677, 440);
+            this.pictureBox_Table.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Table.TabIndex = 2;
+            this.pictureBox_Table.TabStop = false;
+            this.pictureBox_Table.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Table_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 440);
+            this.Controls.Add(this.panel_Table);
+            this.Controls.Add(this.panel_Control);
+            this.Name = "Form1";
+            this.Text = "TexasPoker Caculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel_Control.ResumeLayout(false);
+            this.panel_Control.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_PokerStack)).EndInit();
+            this.panel_Table.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Control;
+        private System.Windows.Forms.PictureBox pictureBox_PokerStack;
+        private System.Windows.Forms.Panel panel_Table;
+        private System.Windows.Forms.PictureBox pictureBox_Table;
+        private System.Windows.Forms.Label label1;
+
+
+    }
+}
+
---
+++ TexasPokerCaculator/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TexasPokerCaculator
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle rect_Player1;
+        private Rectangle rect_Player2;
+        private Rectangle rect_Player3;
+        private Rectangle rect_Player4;
+        private Rectangle rect_Player5;
+        private Rectangle rect_Player6;
+        private Rectangle rect_Player7;
+        private Rectangle rect_Player8;
+        private Rectangle rect_Player9;
+        private Rectangle rect_Player10;
+
+        private const int SmallPokerWidth = 17;
+        private const int SmallPokerHeight = 36;
+        private const int SmallPokerPicTopMargin = 4;
+        private const int SmallPokerPicLeftMargin = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        #region UI functions
+        private void DrawBoxForPlayers(Graphics g)
+        {
+            int tablePicWidth = this.pictureBox_Table.Width;
+            int tablePicHeight = this.pictureBox_Table.Height;
+
+            int centralAreaWidth = (int)(tablePicWidth * 16 / 27.7);
+            int centralAreaHeight = (int)(tablePicHeight * 7.5 / 16);
+
+            int leftMargin = (tablePicWidth - centralAreaWidth) / 2;
+            int topMargin = (tablePicHeight - centralAreaHeight) / 2;
+
+            // Draw next line and...
+            g.DrawRectangle(Pens.Red, leftMargin, topMargin, centralAreaWidth, centralAreaHeight);
+
+            int boxWidth = (int)(centralAreaWidth * 0.8 / 4);
+            int boxHeight = (int)(centralAreaHeight * 0.8 / 2);
+
+            int boxHorizontalGap = (int)(centralAreaWidth * 0.2 / 3);
+
+            //Draw top row boxes
+            rect_Player2 = new Rectangle(leftMargin, topMargin, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player2);
+            
+            rect_Player3 = new Rectangle(leftMargin + boxWidth + boxHorizontalGap, topMargin, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player3);
+            
+            rect_Player4 = new Rectangle(leftMargin + 2 * boxWidth + 2 * boxHorizontalGap, topMargin, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player4);
+            
+            rect_Player5 = new Rectangle(leftMargin + 3 * boxWidth + 3 * boxHorizontalGap, topMargin, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player5);
+
+            //Draw bottom row boxes
+            int boxY = topMargin + centralAreaHeight - boxHeight;
+            rect_Player10 = new Rectangle(leftMargin, boxY, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player10);
+
+            rect_Player9 = new Rectangle(leftMargin + boxWidth + boxHorizontalGap, boxY, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player9);
+
+            rect_Player8 = new Rectangle(leftMargin + 2 * boxWidth + 2 * boxHorizontalGap, boxY, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player8);
+
+            rect_Player7 = new Rectangle(leftMargin + 3 * boxWidth + 3 * boxHorizontalGap, boxY, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player7);
+
+
+            //Draw left column box 
+            int boxX = leftMargin - boxWidth;
+            boxY = topMargin + (int)((centralAreaHeight - boxHeight) / 2);
+            rect_Player1 = new Rectangle(boxX, boxY, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player1);
+
+            //Draw right column box 
+            boxX = leftMargin + centralAreaWidth;
+            rect_Player6 = new Rectangle(boxX, boxY, boxWidth, boxHeight);
+            g.DrawRectangle(Pens.Red, rect_Player6);
+        }
+
+        private void DrawPokerStackHighlightBox()
+        { 
+        
+        }
+        #endregion
+
+
+        private void pictureBox_Table_Paint(object sender, PaintEventArgs e)
+        {
+            DrawBoxForPlayers(e.Graphics);
+        }
+
+        private void pictureBox_PokerStack_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
